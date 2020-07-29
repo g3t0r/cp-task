bdfe1141d85cf833ab7276dbc3900448b5d4a8ac
jan jakubowski <jan.jakubowski@gmail.com>
2020-07-29 16:15:25 +0200

    initial commit

---
+++ src/main/java/com/jakjakubowski/canpack/CanpackApplication.java
@@ -0,0 +1,13 @@
+package com.jakjakubowski.canpack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanpackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanpackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jakjakubowski/canpack/CanpackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakjakubowski.canpack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanpackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
